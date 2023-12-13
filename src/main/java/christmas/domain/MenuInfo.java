03b4cbea404e38965af5eb432adfa2650062d033
SuhHyeonjun <dmsgktn0208@gmail.com>
2023-12-13 20:00:39 +0900

    feat : 메뉴 정보를 갖는 클래스 추가

---
+++ src/main/java/christmas/domain/MenuInfo.java
@@ -0,0 +1,31 @@
+package christmas.domain;
+
+public class MenuInfo {
+    private static final int MENU_NAME_INDEX = 0;
+    private static final int MENU_PRICE_INDEX = 1;
+    private final String name;
+    private final int price;
+
+    public MenuInfo(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public static MenuInfo from(Object[] menu) {
+        String name = (String) menu[MENU_NAME_INDEX];
+        int price = (int) menu[MENU_PRICE_INDEX];
+        return new MenuInfo(name, price);
+    }
+
+    public boolean equalsName(String menuName) {
+        return name.equals(menuName);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}
