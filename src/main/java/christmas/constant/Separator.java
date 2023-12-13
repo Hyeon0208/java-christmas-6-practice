10ed6329a8ae23bf2690e46ac8dff466f87f7130
SuhHyeonjun <dmsgktn0208@gmail.com>
2023-12-13 18:01:23 +0900

    feat : 구분 기호를 상수로 관리하는 Enum클래스 추가

---
+++ src/main/java/christmas/constant/Separator.java
@@ -0,0 +1,19 @@
+package christmas.constant;
+
+public enum Separator {
+    COMMA(",");
+
+    private final String separator;
+
+    Separator(String separator) {
+        this.separator = separator;
+    }
+
+    public String[] split(String value) {
+        return value.split(separator);
+    }
+
+    public String getSeparator() {
+        return separator;
+    }
+}
